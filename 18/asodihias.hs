98f8525683d28e3ed8e92a9554b35ecb50c37351
halvor <halvor@halvor-Latitude-D610.(none)>
2013-04-03 11:07:09 +0200

    fixing shit Revert "added more files"
    This reverts commit f84f636d18c0daf4bb67cdcd73a73b0ee05082b4.

--- 18/asodihias.hs
+++
@@ -1,7 +0,0 @@
-mapFuck :: [[String]] -> [[Int]]
-
-mapFuck [] = []
-mapFuck (h:t) = map read h:mapFuck t
-
-
-(mapFuck (map words (lines input)))
--- 29/solution.hs
+++
@@ -1,5 +0,0 @@
-import Data.List
-
-getTerms :: Integer -> [Integer]
-getTerms n = nub [x^y | x <-[2..n], y <- [2..n]]
-
