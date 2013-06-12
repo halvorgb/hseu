df438dcc789b9145344aeafb7f6ef6d833f83d6a
halvorgb <halvor@halvor-Latitude-D610.(none)>
2013-06-12 20:40:21 +0200

    Removed wrong answers

--- 18/bettersolution.hs
+++ 18/bettersolution.hs
@@ -13,20 +13,9 @@ findLargestSum input = show $ head $ getMaxSum' $ reverse (map (\list -> map rea
 getMaxSum' [root] = root
 getMaxSum' (leaves:root:rest) = getMaxSum' ((zipWith (+) (pruneLeaves leaves) root):rest)
 
--- TOP DOWN: 
-{-
-findLargestSum input = show $ head $ getMaxSum' $ (map (\list -> map read list) (map words (lines input)))
-
-getMaxSum' (root:leaves)
-  | leaves == [] = pruneLeaves root
-  | otherwise = zipWith (+) (getMaxSum' leaves) (pruneLeaves root)
--}
-
--- CALLED BY BOTH
 pruneLeaves :: [Int] -> [Int]
 
 pruneLeaves [] = []
-
 pruneLeaves (left:right:rest) 
   | rest == [] = (max left right):pruneLeaves rest
   | otherwise = (max left right):pruneLeaves (right:rest)
--- 31/solution.hs
+++ 31/solution.hs
@@ -1,6 +1,6 @@
 import Data.List
 import Data.Maybe
-
+-- FEIL
 values = [1, 2, 5, 10, 20, 50, 100, 200]
 
 solve n = solveShit (fromJust (elemIndex n values))
